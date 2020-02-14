efc6c02a630f063e11af741befbb251f6b21029f
rizkifani25 <rizkifani11@gmail.com>
2020-02-14 13:53:38 +0700

    setting port

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(endpoint.LIST_ALPRO_ICONS, alproIconsRouter);
 app.use(endpoint.LIST_ALPRO_CABLE, alproCatCableRouter);
 app.use(endpoint.LIST_ALPRO_ODC, alproCatODCRouter);
 app.use(endpoint.LIST_ALPRO_ODP, alproCatODPRouter);
-app.use(endpoint.LIST_ALPRO_POST, alproCatODPRouter);
+app.use(endpoint.LIST_ALPRO_POST, alproCatPostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -75,7 +75,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "production" ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
