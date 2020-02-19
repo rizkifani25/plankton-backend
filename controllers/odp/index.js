888eb625cd0d3a04b7bb91f531845b85b08d696c
carinama11 <jarpani31@gmail.com>
2020-02-19 14:59:23 +0700

    closest node

--- controllers/odp/index.js
+++ controllers/odp/index.js
@@ -1,13 +1,59 @@
 const odpModel = require("../../models/odp");
 
-exports.allODP = (req, res) => {
+const FindClosestNode = (origin, points) => {
+  let closestDistance = Infinity;
+  let closestPoint;
+
+  points.map(point => {
+    const deltaX = Math.pow(
+      parseFloat(origin.LATITUDE) - parseFloat(point.LATITUDE),
+      2
+    );
+    const deltaY = Math.pow(
+      parseFloat(origin.LONGITUDE) - parseFloat(point.LONGITUDE),
+      2
+    );
+    const jarak = Math.sqrt(deltaX + deltaY);
+    if (jarak < closestDistance) {
+      closestDistance = jarak;
+      closestPoint = point;
+    }
+  });
+
+  return { closestPoint, closestDistance };
+};
+
+exports.closestODP = (req, res) => {
+  const query = { LATITUDE: "-6,9984", LONGITUDE: "107,1410" };
+
   odpModel
-    .find({})
+    .find({
+      LATITUDE: { $lt: query.LATITUDE },
+      LONGITUDE: { $lt: query.LONGITUDE }
+    })
+    .sort({ LATITUDE: -1, LONGITUDE: -1 })
+    .limit(1000)
     .exec()
-    .then(data => {
-      res.status(200).send({
-        data: data
-      });
+    .then(closestLess => {
+      odpModel
+        .find({
+          LATITUDE: { $gt: query.LATITUDE },
+          LONGITUDE: { $gt: query.LONGITUDE }
+        })
+        .sort({ LATITUDE: 1, LONGITUDE: 1 })
+        .limit(1000)
+        .exec()
+        .then(closestMore => {
+          const less = FindClosestNode(query, closestLess);
+          const more = FindClosestNode(query, closestMore);
+          console.log(less, more);
+
+          if (less.closestDistance < more.closestDistance) {
+            res.status(200).send({ data: less.closestPoint });
+          } else {
+            res.status(200).send({ data: more.closestPoint });
+          }
+        });
     })
     .catch(err => {
       res.send({
--- controllers/report/index.js
+++ controllers/report/index.js
@@ -2,20 +2,18 @@ const reportSchema = require("../../models/reports");
 const mongoose = require("mongoose");
 
 exports.uploadReport = async (req, res) => {
-  const { user, coords, alproType, detail, description, rawImage } = req.query;
-  let image_url = "";
+  const { image_url, user, alproType, detail, description, coords } = req.query;
 
   // const image_url = handleUploadImage(rawImage); //return imageUrl from firebase
-  console.log(req.query, req.body);
-
+  console.log(req.query.description);
   const newReport = new reportSchema({
     _id: `PL${new mongoose.Types.ObjectId()}`,
-    user,
-    alproType,
+    user: JSON.parse(user),
+    alproType: JSON.parse(alproType),
     detail,
     description,
     image_path: image_url || "/",
-    location: coords
+    location: JSON.parse(coords)
   });
 
   console.log(newReport);
