6da15f65a45489ce26c14a26dc269440220d196a
Muhammad Rizki Fani <rizkifani11@gmail.com>
2020-03-18 13:10:06 +0700

    fix route

--- routes/overview/index.js
+++ routes/overview/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 const overview = require("../../controllers/filter");
 
-router.get("/", overview.countReportByRegional);
+router.get("/", overview.testingOverview);
 
 module.exports = router;
